46b07780065587b19c1fafe456adcccc5b169779
Antoine07 <antoine.lucsko@gmail.com>
2024-11-20 12:34:57 +0100

    courses module

--- src/DataFixtures/EducationFixtures.php
+++ src/DataFixtures/EducationFixtures.php
@@ -6,6 +6,7 @@ use App\Entity\Course;
 use App\Factory\AddressFactory;
 use App\Factory\CourseFactory;
 use App\Factory\EducationFactory;
+use App\Factory\ModuleFactory;
 use App\Factory\TitleFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
@@ -18,75 +19,66 @@ class EducationFixtures extends Fixture
             "Introduction au Full Stack Development",
             "Développement Frontend avec React",
             "Architecture Backend avec Node.js",
-            "Gestion de bases de données SQL et NoSQL",
-            "Déploiement d'applications Full Stack"
         ],
         "CDA" => [
             "Introduction à la Certification de Compétences en Développement",
             "Gestion de projets agiles pour développeurs",
             "Design patterns et principes SOLID",
-            "Tests unitaires et tests d'intégration",
-            "Sécurité des applications web"
         ],
         "ESI" => [
             "Fondamentaux de l'Ingénierie des Systèmes d'Information",
             "Architecture des systèmes d'information",
-            "Gestion de projets SI et méthodologie Agile",
-            "Sécurité des systèmes d'information",
-            "Maintenance et évolution des SI"
         ],
         "DATA" => [
             "Introduction à la Data Science",
             "Big Data et gestion des données volumineuses",
-            "Analyse de données avec Python",
-            "Modélisation et algorithmes de Machine Learning",
-            "Data Visualization avec Power BI et Tableau"
         ],
         "AIA" => [
             "Introduction à l'Intelligence Artificielle",
             "Apprentissage supervisé et non supervisé",
-            "Deep Learning et réseaux neuronaux",
-            "Applications de l'IA dans le secteur privé",
-            "Ethique et impact social de l'Intelligence Artificielle"
         ]
     ];
-
-    private array $titles = ["FSD", "CDA", "ESI", "DATA", "AIA"];
 
     public function load(ObjectManager $manager): void
     {
 
-        foreach (['3WA', 'EM', 'HETIC', 'ARCPLEX'] as $name) {
+        foreach (['3WA', 'EM', 'HETIC'] as $name) {
 
-            $randomKeys = array_rand($this->titles, count($this->titles));
-
-            $randonTitles = [];
-            foreach ($randomKeys as $key) {
-                $randonTitles[] = $this->titles[$key];
-            }
-
-            $titles = [];
-            foreach ($randonTitles as $title) {
-                $titles[] = TitleFactory::createOne(['name' => $title]);
-            }
-
-            // création des courses en fonction des titres
-
-            $courses = [];
-            foreach ($randonTitles as $title) {
-                foreach ($this->courseTitles[$title] as $courseTitle) {
-                   $courses[] = CourseFactory::createOne(['title' => $courseTitle]);
+            [$titles, $courses] = [[], []];
+            foreach ($this->randomValues(["FSD", "CDA", "ESI", "DATA"]) as $t) {
+                $titles[] = TitleFactory::createOne(['name' => $t]);
+                foreach ($this->courseTitles[$t] as $courseTitle) {
+                    $courses[] = CourseFactory::createOne(['title' => $courseTitle]);
                 }
             }
 
-            $education = EducationFactory::createOne([
+            EducationFactory::createOne([
                 'name' => $name,
                 'titles' => $titles,
                 'address' => AddressFactory::createOne(),
                 'courses' => $courses
             ]);
 
-            
+            foreach (CourseFactory::all() as $course) {
+
+                ModuleFactory::createMany(rand(1, 2), function () use ($course) {
+
+                    return [
+                        'courses' => [$course]
+                    ];
+                });
+            }
         }
+    }
+
+    private function randomValues(array $data, int $m = 2)
+    {
+
+        $randomKeys = array_rand($data, $m);
+        $alea = [];
+        foreach ($randomKeys as $key)
+            $alea[] = $data[$key];
+
+        return $alea;
     }
 }
